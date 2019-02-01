5c4ac1e02334af5172a87a3a1675dee6f5a040c8
FreitagDavid <freitagdavid@outlook.com>
2019-02-01 12:19:16 -0500

    api/server: GET /api/games is passing basic test

--- api/server.js
+++ api/server.js
@@ -3,3 +3,10 @@ const games = require('./gamesModel');
 const server = express();
 
 server.use(express.json());
+
+server.get('/api/games', (req, res) => {
+    const data = games.get();
+    res.status(200).json(data);
+});
+
+module.exports = server;
