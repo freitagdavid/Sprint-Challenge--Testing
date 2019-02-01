aac54e63629e390c1701d53efd81ae307df85d4f
FreitagDavid <freitagdavid@outlook.com>
2019-02-01 14:02:15 -0500

    api/server.spec: I think this should be all the tests

--- api/server.spec.js
+++ api/server.spec.js
@@ -12,23 +12,35 @@ describe('server.js', () => {
             let response = await request(server).get('/api/games');
             expect(response.type).toMatch(/json/i);
         });
+        it('Respond with array of games', async () => {
+            let response = await request(server).get('/api/games');
+            expect(Array.isArray(response.body)).toBeTruthy();
+        });
     });
     describe('POST /api/games', () => {
+        const testData = {
+            title: 'Zork',
+            genre: 'Text based adventure',
+            releaseYear: 1977,
+        };
         it('Respond with status code 201', async () => {
-            let response = await request(server).post('/api/games', {
-                title: 'Zork',
-                genre: 'Text base adventure',
-                releaseYear: 1977,
-            });
+            let response = await request(server)
+                .post('/api/games')
+                .send(testData);
             expect(response.status).toBe(201);
         });
-        it('Respond with updated data', async () => {
-            let response = await request(server).post('/api/games', {
-                title: 'Zork',
-                genre: 'Text base adventure',
-                releaseYear: 1977,
-            });
+        it('Respond with json', async () => {
+            let response = await request(server)
+                .post('/api/games')
+                .send(testData);
             expect(response.type).toMatch(/json/i);
+        });
+        it('Respond with inserted data', async () => {
+            let response = await request(server)
+                .post('/api/games')
+                .send(testData);
+            const { body } = response;
+            expect(body.title === testData.title).toBeTruthy();
         });
     });
 });
