79e4b6477a8763118257a01b1a3489706991c3a1
FreitagDavid <freitagdavid@outlook.com>
2019-02-01 12:08:16 -0500

    api/server.spec: Tests for the get on /api/games

--- api/server.spec.js
+++ api/server.spec.js
@@ -2,4 +2,16 @@ const request = require('supertest');
 
 const server = require('./server.js');
 
-describe('server.js', () => {});
+describe('server.js', () => {
+    describe('GET /api/games', () => {
+        it('Respond with status code 200', async () => {
+            let response = await request(server).get('/api/games');
+            expect(response.status).toBe(200);
+        });
+        it('Respond with a json object', async () => {
+            let response = await request(server).get('/api/games');
+            expect(response.type).toMatch(/json/i);
+        });
+    });
+    describe('POST /api/games', () => {});
+});
